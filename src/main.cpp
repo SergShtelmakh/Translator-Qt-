a41ef07378892dedfb3e203a94ec92adccbc80ee
SergShtelmakh <sh.serg1993@gmail.com>
2015-03-16 16:12:09 +0200

    Solving plugins problem

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QDir>
 
 int main(int argc, char *argv[])
 {
+    QDir appDir = QFileInfo(QString::fromLocal8Bit(argv[0])).absoluteDir();
+    QString libPath = appDir.absolutePath()+"/plugins";
+    QApplication::addLibraryPath(libPath);
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
